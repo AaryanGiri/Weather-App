b3927e56cdef3808a713ad6fd5190cc2b92b060f
Aaryan Giri <aaryangir@gmail.com>
2022-07-07 10:22:28 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "772b59b98bb2b91bed5dddbc16d2dfe1"
+    }
+}
+
+module.exports = constants;
